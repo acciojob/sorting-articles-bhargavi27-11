0fc232b56399945b5585e97ab5eab62099e85071
sesetty bhargavi <sesettybhargavi@gmail.com>
2026-03-20 18:15:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const bands =  [
+	'The Plot in You',
+	'The Devil Wears Prada',
+	'Pierce the Veil',
+	'Norma Jean',
+	'The Bled',
+	'Say Anything',
+	'The Midway State',
+	'We Came as Romans',
+	'Counterparts',
+	'Oh, Sleeper',
+	'A Skylit Drive',
+	'Anywhere But Here',
+	'An Old Dog'
+];
+
+function strip(bandName) {
+	return bandName.replace(/^(a|an|the)/i,'').trim();
+}
+
+const sortedBands = bands.sort((a,b) => {
+	if(strip(a) > strip(b)) return 1;
+	if(strip(a) < strip(b)) return -1;
+	return 0;
+});
+
+const bandList = document.getElementById("band");
+
+sortedBands.forEach((band) => {
+	const li = document.createElement('li');
+	li.textContent = band;
+	bandList.appendChild(li);
+});
+
+
+
+
